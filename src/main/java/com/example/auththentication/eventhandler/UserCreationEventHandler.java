a05f3f25fcae8b24903f219926f5767099f4b753
Rajendra Kumar Mahapatra <rmahapatra1999@gmail.com>
2024-06-21 15:14:20 +0530

    mail service implemented using java mail sender

--- src/main/java/com/example/auththentication/eventhandler/UserCreationEventHandler.java
+++ src/main/java/com/example/auththentication/eventhandler/UserCreationEventHandler.java
@@ -3,17 +3,21 @@ package com.example.auththentication.eventhandler;
 import com.example.auththentication.dto.event.UserCreationEvent;
 import com.example.auththentication.model.User;
 import com.example.auththentication.service.MailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserCreationEventHandler {
+    private final MailService mailService;
+
 
     @Async
     @EventListener
     public void handleUserCreationEvent (UserCreationEvent event) {
         User user = event.getUser();
-        MailService.sendUserCreationMail(user);
+        mailService.sendUserCreationMail(user);
     }
 }
--- src/main/java/com/example/auththentication/service/MailService.java
+++ src/main/java/com/example/auththentication/service/MailService.java
@@ -1,76 +1,71 @@
 package com.example.auththentication.service;
 
 import com.example.auththentication.model.User;
+import jakarta.activation.DataSource;
+import jakarta.activation.FileDataSource;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.Properties;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
+@RequiredArgsConstructor
+@Service
 public class MailService {
+    private final JavaMailSender emailSender;
 
-    /**
-     * Utility method to send simple HTML email
-     * @param session
-     * @param toEmail
-     * @param subject
-     * @param body
-     */
-    public static void sendEmail(Session session, String toEmail, String subject, String body){
-        try
-        {
-            MimeMessage msg = new MimeMessage(session);
-            //set message headers
-            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-            msg.addHeader("format", "flowed");
-            msg.addHeader("Content-Transfer-Encoding", "8bit");
+    public void sendMail(String to, String subject, String text) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-            msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
-            msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
-            msg.setSubject(subject, "UTF-8");
-            msg.setText(body, "UTF-8");
-            msg.setSentDate(new Date());
-            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
-
-            System.out.println("Message is ready");
-            Transport.send(msg);
-
-            System.out.println("EMail Sent Successfully!!");
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        helper.setFrom("rajumahapatra096@gmail.com", "Registration-service");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        emailSender.send(message);
+        System.out.println("Email Sent Successfully!!");
     }
 
-    public static void sendUserCreationMail(User user) {
-        final String fromEmail = "rajumahapatra096@gmail.com";
-        final String password = "rlsfdjotmrbsenux";
-        final String toEmail = user.getEmail();
 
-        System.out.println("TLSEmail Start");
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+    public void sendMail(String to, String subject, String text, List<String> filePaths) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        //create Authenticator object to pass in Session.getInstance argument
-        Authenticator auth = new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(fromEmail, password);
+        helper.setFrom("rajumahapatra096@gmail.com", "Registration-service");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+
+        for (String filePath : filePaths) {
+            File file = new File(filePath);
+            if (file.exists()) {
+                DataSource dataSource = new FileDataSource(file);
+                helper.addAttachment(file.getName(), dataSource);
+            } else {
+                System.err.println("File not found: " + filePath);
             }
-        };
-        Session session = Session.getInstance(props, auth);
+        }
+
+        emailSender.send(message);
+        System.out.println("Email Sent Successfully!!");
+    }
+
+    public void sendUserCreationMail(User user) {
         String subject = "User registered successfully";
         String body = String.format("Your user account with %s email have been created successfully!", user.getEmail());
-
-        sendEmail(session, toEmail,subject, body);
+        String attachedFilePath = "D:/PersonalDocuments/My_document/Aadhaar_card.pdf";
+        List<String> filePaths = new ArrayList<>();
+        filePaths.add(attachedFilePath);
+        try {
+            sendMail(user.getEmail(), subject, body, filePaths);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
